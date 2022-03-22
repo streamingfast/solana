b7e4d9a2d381d634a0cfacdc46c8c0f8e0a059cf
Julien Cassis <juliencassis@gmail.com>
2022-03-22 18:27:04 -0400

    fix bug

--- runtime/src/message_processor.rs
+++ runtime/src/message_processor.rs
@@ -1507,7 +1507,7 @@ impl MessageProcessor {
         // DMLOG: This is the call entry point for top level instructions
         //****************************************************************
         if let Some(ctx_ref) = &dmbatch_context {
-            // let ctx = ctx_ref.deref();
+            let ctx = ctx_ref.deref();
             // if let Some(log_collector) = log_collector {
             //     let logs = log_collector.get_instruction_message();
             //     ctx.borrow_mut().set_instruction_logs(logs);
